5f3ad537b1035d407e4e0405df8fa9cef8fca03a
awadhesh06 <71342190+awadhesh06@users.noreply.github.com>
2022-11-10 22:51:36 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("Hello everyone! ,jenkins integration");
+		System.out.println("Hello everyone! ,jenkins integration with github webhooks");
 }
 }
